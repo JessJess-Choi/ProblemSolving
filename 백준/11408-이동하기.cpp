54c95ad06dae7398ffa27bdeddcc8a582619223a
JaeSung Choi <85390127+JessJess-Choi@users.noreply.github.com>
2021-08-03 17:06:48 +0900

    Create 11408-이동하기.cpp

---
+++ 백준/11408-이동하기.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int dp[1001][1001];
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n,m;cin>>n>>m;
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=m;j++)
+            cin>>dp[i][j];
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=m;j++)
+            dp[i][j] += max(max(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]);
+    cout<<dp[n][m];
+    return 0;
+}
