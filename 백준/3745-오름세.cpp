ebea20f28ff9b46bcd5ea135e0b6949d8b4320d9
JaeSung Choi <85390127+JessJess-Choi@users.noreply.github.com>
2021-08-10 16:52:37 +0900

    Create 3745-오름세.cpp

---
+++ 백준/3745-오름세.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#define INF 1e9
+using namespace std;
+int arr[1000001];
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+	while(cin>>n){
+		for(int i=0;i<n;i++)cin>>arr[i];
+		vector<int> v;
+		v.push_back(-INF);
+		for(int i=0;i<n;i++)
+			if(v.back() < arr[i])v.push_back(arr[i]);
+			else{
+				auto it = lower_bound(v.begin(),v.end(),arr[i]);
+				*it = arr[i];
+			}
+		cout<<v.size()-1<<'\n';
+	}
+    return 0;
+}
