70f39f2bbd4b44e7a79568aeefac085cc75e9f22
최재성(JaeSung Choi) <85390127+JessJess-Choi@users.noreply.github.com>
2022-05-16 16:03:15 +0900

    Create 예산.cpp

---
+++ 프로그래머스/예산.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(),d.end());
+    for(auto a : d){
+        budget -= a;
+        if(budget < 0)break;
+        answer++;
+    }
+    return answer;
+}
