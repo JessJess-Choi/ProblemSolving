3b1c00cec9e58b0ad1393c18d4eda1f3a6651741
최재성(JaeSung Choi) <85390127+JessJess-Choi@users.noreply.github.com>
2022-05-24 11:11:02 +0900

    Create 같은 숫자는 싫어.cpp

---
+++ 프로그래머스/같은 숫자는 싫어.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    for(int i=0;i<arr.size();i++){
+        answer.push_back(arr[i]);
+        while(i < arr.size() && arr[i] == arr[i + 1])i++;
+    }
+    return answer;
+}
