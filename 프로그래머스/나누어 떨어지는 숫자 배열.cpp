7284b208d0457e06ea174f224d812518ef8e3df4
최재성(JaeSung Choi) <85390127+JessJess-Choi@users.noreply.github.com>
2022-05-26 13:31:50 +0900

    Create 나누어 떨어지는 숫자 배열.cpp

---
+++ 프로그래머스/나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for(auto a : arr)
+        if(a % divisor == 0)
+            answer.push_back(a);
+    if(answer.empty())
+        answer.push_back(-1);
+    else
+        sort(answer.begin(),answer.end());
+    return answer;
+}
