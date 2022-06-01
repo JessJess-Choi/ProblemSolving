678f32715d08d7f8bbec7fc001ca2a81c64a121b
최재성(JaeSung Choi) <85390127+JessJess-Choi@users.noreply.github.com>
2022-06-01 12:32:37 +0900

    Create 문자열 다루기 기본.cpp

---
+++ 프로그래머스/문자열 다루기 기본.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+using namespace std;
+
+bool solution(string s) {
+    if(s.size() != 4 && s.size() != 6)return false;
+    for(auto a : s)
+        if(a < '0' || a > '9')return false;
+    return true;
+}
